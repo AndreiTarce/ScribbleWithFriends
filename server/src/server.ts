02f5139f85d73bf9fa8c007f5f353696a3d9ca55
AndreiTarce <tarceandrei@gmail.com>
2024-04-12 16:44:26 +0300

    set up express server and socket.io

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { Server, type Socket } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket: Socket) => {
+    console.log("connection");
+});
+
+httpServer.listen(3000);
